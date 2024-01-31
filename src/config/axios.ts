abfbe1eceb6327ec5d7ada99b3aa21e7ac9aea32
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-01-31 17:36:21 +0100

    feat: Removing unused files and dependencies ❌

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-export const mapApi = axios.create({
-  baseURL: 'https://photon.komoot.io/api',
-});
-
 export const placeApi = axios.create({
   baseURL: 'https://nominatim.openstreetmap.org',
 });
