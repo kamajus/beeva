8577ba04e10f57e6eca188037ac64e74445a33c4
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-02-03 17:09:39 +0100

    feat: Add LoadScreen

---
+++ src/components/LoadScreen.tsx
@@ -0,0 +1,11 @@
+import { ActivityIndicator, View } from 'react-native';
+
+import Constants from '../constants';
+
+export default function LoadScreen() {
+  return (
+    <View className="bg-white w-full h-full flex justify-center items-center">
+      <ActivityIndicator animating color={Constants.colors.primary} size={40} />
+    </View>
+  );
+}
