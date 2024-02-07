775a1c4a6fb52db3d2af1d15ea88d2a11e399d45
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-02-07 17:47:16 +0100

    fix: Avatar Image Upload

--- src/app/residence/[id].tsx
+++ src/app/residence/[id].tsx
@@ -114,7 +114,10 @@ export default function ResidenceDetail() {
           <View className="flex gap-x-3 flex-row">
             <>
               {user?.photo_url ? (
-                <Avatar.Image size={50} source={{ uri: user.photo_url }} />
+                <Avatar.Image
+                  size={50}
+                  source={{ uri: user.photo_url + '?timestamp=' + new Date().getTime() }}
+                />
               ) : (
                 <Avatar.Text size={50} label={String(user?.first_name[0])} />
               )}
