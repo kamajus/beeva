263e1a6552e4f91cd9744e4bfb435548b797cea5
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-09-04 22:48:57 +0200

    feat: Notification read functionality

--- src/components/ActionSheet/index.ts
+++ src/components/ActionSheet/index.ts
@@ -3,13 +3,15 @@ import { SheetDefinition, registerSheet } from 'react-native-actions-sheet'
 import CreateWisheActionSheet from './CreateWisheActionSheet'
 import DeleteActionSheet from './DeleteActionSheet'
 import FilterActionSheet from './FilterActionSheet'
+import NotificationMenuSheet from './NotificationMenuSheet'
 import ResidenceMenuSheet from './ResidenceMenuSheet'
 
-import { IResidence } from '@/@types'
+import { INotification, IResidence } from '@/@types'
 
 registerSheet('filter-search-sheet', FilterActionSheet)
 registerSheet('create-wishe-sheet', CreateWisheActionSheet)
 registerSheet('account-delete-sheet', DeleteActionSheet)
+registerSheet('notification-menu-sheet', NotificationMenuSheet)
 registerSheet('residence-menu-sheet', ResidenceMenuSheet)
 
 declare module 'react-native-actions-sheet' {
@@ -17,6 +19,12 @@ declare module 'react-native-actions-sheet' {
     'residence-menu-sheet': SheetDefinition<{
       payload: {
         residence: IResidence
+      }
+    }>
+
+    'notification-menu-sheet': SheetDefinition<{
+      payload: {
+        notification: INotification
       }
     }>
   }
