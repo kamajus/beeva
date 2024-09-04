5039328df6e1424f8fa80b7cb885c85d590f70c6
Carlos Mateus <carlosmatateumateus@gmail.com>
2024-09-04 22:48:57 +0200

    feat: Notification read functionality

--- src/components/NotificationBox.tsx
+++ src/components/NotificationBox.tsx
@@ -3,13 +3,15 @@ import { router } from 'expo-router'
 import { icons } from 'lucide-react-native'
 import { ReactNode, useEffect, useMemo, useState } from 'react'
 import { Text, View } from 'react-native'
-import { SheetProvider } from 'react-native-actions-sheet'
+import { SheetManager, SheetProvider } from 'react-native-actions-sheet'
 
 import { INotification } from '@/@types'
 import PublishedSince from '@/components/PublishedSince'
 import TouchableBrightness from '@/components/TouchableBrightness'
 import constants from '@/constants'
+import { NotificationRepository } from '@/repositories/notification.repository'
 import { ResidenceNotificationRepository } from '@/repositories/residence.notification.repository'
+import { useNotificationStore } from '@/store/NotificationStore'
 import { useResidenceNotificationStore } from '@/store/ResidenceNotificationStore'
 
 interface INotificationIcons {
@@ -25,21 +27,21 @@ const notificationIcons: INotificationIcons = {
 
 interface INotificationBoxLink {
   children: ReactNode
-  notificationId: string
-  notificationType: INotification['type']
+  notification: INotification
 }
 
 const NotificationBoxLink = ({
   children,
-  notificationType,
-  notificationId,
+  notification,
 }: INotificationBoxLink) => {
   const getByNotificationId = useResidenceNotificationStore(
     (state) => state.getByNotificationId,
   )
+  const addNotification = useNotificationStore((state) => state.add)
   const addResidenceNotification = useResidenceNotificationStore(
     (state) => state.add,
   )
+  const notificationRepository = useMemo(() => new NotificationRepository(), [])
   const residenceNotificationRepository = useMemo(
     () => new ResidenceNotificationRepository(),
     [],
@@ -48,15 +50,15 @@ const NotificationBoxLink = ({
 
   useEffect(() => {
     if (
-      notificationType === 'residence-posted' ||
-      notificationType === 'wishe-found'
+      notification.type === 'residence-posted' ||
+      notification.type === 'wishe-found'
     ) {
       const fetchData = async () => {
-        const residenceNotification = getByNotificationId(notificationId)
+        const residenceNotification = getByNotificationId(notification.id)
 
         if (!residenceNotification) {
           const data = await residenceNotificationRepository.find({
-            notification_id: notificationId,
+            notification_id: notification.id,
           })
 
           if (data.length !== 0) {
@@ -73,26 +75,42 @@ const NotificationBoxLink = ({
       fetchData()
     }
   }, [
-    notificationType,
-    notificationId,
+    notification,
     residenceNotificationRepository,
     getByNotificationId,
     addResidenceNotification,
   ])
 
-  const handlePress = () => {
+  async function handlePress(notification: INotification) {
+    await notificationRepository.update(notification.id, {
+      was_readed: true,
+    })
+
+    addNotification({
+      ...notification,
+      was_readed: true,
+    })
+
     if (residenceId) {
       router.push(`/residence/${residenceId}`)
     }
   }
 
   return (
-    <TouchableBrightness onPress={handlePress}>{children}</TouchableBrightness>
+    <TouchableBrightness
+      onPress={() => handlePress(notification)}
+      onLongPress={() => {
+        SheetManager.show('notification-menu-sheet', {
+          payload: { notification },
+        })
+      }}>
+      {children}
+    </TouchableBrightness>
   )
 }
 
-export default function NotificationBox(props: INotification) {
-  const icon = notificationIcons[props.type]
+export default function NotificationBox(notification: INotification) {
+  const icon = notificationIcons[notification.type]
   const LucideIcon = icons[icon]
 
   if (!LucideIcon) {
@@ -102,9 +120,7 @@ export default function NotificationBox(props: INotification) {
   return (
     <View>
       <SheetProvider>
-        <NotificationBoxLink
-          notificationId={props.id}
-          notificationType={props.type}>
+        <NotificationBoxLink notification={notification}>
           <View className="w-full px-2 py-2 flex items-center flex-row border-b border-gray-300">
             <View className="relative">
               <LucideIcon color={constants.colors.primary} size={30} />
@@ -112,7 +128,7 @@ export default function NotificationBox(props: INotification) {
                 className={clsx(
                   'absolute bottom-7 left-7 bg-[#e83f5b] rounded-full flex justify-center items-center w-2 h-2',
                   {
-                    hidden: props.was_readed,
+                    hidden: notification.was_readed,
                   },
                 )}
               />
@@ -120,12 +136,12 @@ export default function NotificationBox(props: INotification) {
 
             <View className="p-4 pr-8">
               <Text className="font-poppins-medium text-sm leading-relaxed text-black">
-                {props.description}
+                {notification.description}
               </Text>
 
               <PublishedSince
                 className="mt-1 font-poppins-medium text-xs text-gray-500"
-                date={props.created_at}
+                date={notification.created_at}
               />
             </View>
           </View>
